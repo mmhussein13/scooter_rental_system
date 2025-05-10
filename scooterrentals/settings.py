ea432f545852a57d6b2b1faa80b6eb8c13774708
Hussein <h@Husseins-MBP.lan>
2025-05-10 21:14:15 +0200

    script for 5 minutes inactivity logout

--- scooterrentals/settings.py
+++ scooterrentals/settings.py
@@ -116,3 +116,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+# Session expires after 5 minutes of inactivity
+SESSION_COOKIE_AGE = 300  # 5 minutes in seconds
+
+# Reset expiry time on every request
+SESSION_SAVE_EVERY_REQUEST = True
+
